d912bfed9a44fbf56a2d4698660d22dfb0584102
kar-yeow <kar.yeow@gmail.com>
2023-08-29 09:26:11 +1000

    Updated to include AWS CDK (#1)
    Co-authored-by: Kar Yeow <kar.yeow@ato.gov.au>

---
+++ src/MyCdk/Program.cs
@@ -0,0 +1,23 @@
+﻿using Amazon.CDK;
+
+namespace MyCdk
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new MyCdkStack(app, "MyCdkStack", new StackProps
+            {
+                StackName = $"deploy-codebuild-stack",
+                Env = new Amazon.CDK.Environment
+                {
+                    Account = System.Environment.GetEnvironmentVariable("AWS_ACCOUNT"),
+                    Region = System.Environment.GetEnvironmentVariable("AWS_REGION")
+                }
+            });
+
+            app.Synth();
+        }
+    }
+}
