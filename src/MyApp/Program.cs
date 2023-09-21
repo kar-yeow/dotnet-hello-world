7d442f601c4ad6a115334e8a165664285a26aaaa
Kar Yeow <kar.yeow@ato.gov.au>
2023-09-21 13:37:31 +1000

    Added another rest method

--- src/MyApp/Program.cs
+++ src/MyApp/Program.cs
@@ -2,5 +2,9 @@
 var app = builder.Build();
 var what = System.Environment.GetEnvironmentVariable("HELLO_VAR");
 app.MapGet("/", () => $"Hello World {what}!");
+app.MapGet("/hello/{name}", (string name) => new 
+{
+    Name = name
+});
 
 app.Run();
