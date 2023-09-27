58f863fc673286e0912ddd13e1f19fef5d5d0030
Kar Yeow <kar.yeow@ato.gov.au>
2023-09-28 07:47:21 +1000

    Updated to lookup subnets

--- src/MyCdk/MyDummyStack.cs
+++ src/MyCdk/MyDummyStack.cs
@@ -8,27 +8,38 @@ namespace MyCdk
     {
         public MyDummyStack(Construct scope, string id, IStackProps? props = null) : base(scope,id, props)
         {
+            //var vpc = new Vpc(this, "MyVpc", new VpcProps
+            //{
+            //    RestrictDefaultSecurityGroup = false,
+            //    EnableDnsSupport = true,
+            //    EnableDnsHostnames = true,
+            //    NatGateways = 0,
+            //    IpAddresses = IpAddresses.Cidr("10.0.0.0/20"),
+            //    SubnetConfiguration = new SubnetConfiguration[]
+            //    {
+            //        new SubnetConfiguration
+            //        {
+            //            SubnetType = SubnetType.PRIVATE_ISOLATED,
+            //            CidrMask = 22,
+            //            Name = "my-isolated-subnet"
+            //        }
+            //    },
+            //    MaxAzs = 2
+            //});
 
-            var vpc = new Vpc(this, "MyVpc", new VpcProps
+            var subnets = new ISubnet[]
             {
-                RestrictDefaultSecurityGroup = false,
-                EnableDnsSupport = true,
-                EnableDnsHostnames = true,
-                NatGateways = 0,
-                IpAddresses = IpAddresses.Cidr("10.0.0.0/20"),
-                SubnetConfiguration = new SubnetConfiguration[]
+                Subnet.FromSubnetAttributes(this, "MySubnet", new SubnetAttributes
                 {
-                    new SubnetConfiguration
-                    {
-                        SubnetType = SubnetType.PRIVATE_ISOLATED,
-                        CidrMask = 22,
-                        Name = "my-isolated-subnet"
-                    }
-                },
-                MaxAzs = 2
-            });
+                    AvailabilityZone = "ap-southeast-2a"
+                }),
+                Subnet.FromSubnetAttributes(this, "MySubnet", new SubnetAttributes
+                {
+                    AvailabilityZone = "ap-southeast-2b"
+                })
+            };
 
-            _ = new MyCheckingStack(scope, vpc.IsolatedSubnets, "MyCheckingStack", props);
+            _ = new MyCheckingStack(scope, subnets, "MyCheckingStack", props);
         }
     }
 }
